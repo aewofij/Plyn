768db9539f21ad21bc1912039d57fe9f40529b3d
David Lee <aewofij@gmail.com>
2015-04-05 17:22:30 -0400

    add components through UI

--- js/ui/obj-list-controller.js
+++ js/ui/obj-list-controller.js
@@ -6,25 +6,8 @@ define([ 'underscore' ],
        function (_) {
 
   function ObjListController (theScene) {
-    var self = this;
-
     this.scene = theScene;
-    var updateObjectList = function () {
-      self.allObjects = _.map(_.values(self.scene.objects), function (obj) {
-        return {
-          model: obj,
-          components: _.map(_.keys(obj.components), function (compKey) {
-            return {
-              id: compKey,
-              component: obj.components[compKey]
-            };
-          })
-        };
-      });
-    };
-
-    updateObjectList();
-    Object.observe(this.scene.objects, updateObjectList);
+    this.updateObjectList();
   }
 
   ObjListController.prototype = {
@@ -35,10 +18,16 @@ define([ 'underscore' ],
 
     addObject: function () {
       return this.scene.addObject();
+      this.updateObjectList();
     },
 
     addComponent: function (componentCons, onObj) {
       new componentCons(onObj);
+      this.updateObjectList();
+    },
+
+    updateObjectList: function () {
+      this.allObjects = _.values(this.scene.objects);
     }
 
   }
